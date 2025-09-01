dac28e78719252e8d50bfa7d27cba0ad2a5f2599
jarlagiovana <jarlagiovana2@gmail.com>
2025-09-01 20:29:34 -0300

    Add files via upload

---
+++ array.py
@@ -0,0 +1,16 @@
+#lista de colegas
+listacolegas = ["Victor", "João Pedro", "Rosemeire", "Dayson", "Roni"]
+print("primeiro nome:",listacolegas[0])
+print("segundo nome:", listacolegas[1])
+print("terceiro nome:",listacolegas[2])
+print("quarto nome:",listacolegas[3])
+print("quinto nome:",listacolegas[4])
+#indice           0            1             2           3        4
+
+#desafio com IF
+if "Carla" in listacolegas:
+    print("Carla foi encontrada na lista!")
+else:
+    print("Carla não foi encontrada na lista.")
+
+    
